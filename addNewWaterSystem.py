8ade991a9a229663bf660e1093476a64fbc5fa3b
sd33p <158218664+sd33p@users.noreply.github.com>
2025-04-17 12:59:34 -0500

    Create addNewWaterSystem.py

---
+++ addNewWaterSystem.py
@@ -0,0 +1,45 @@
+# request library to interact with the server/api
+# json to make the resulting json into a redable format
+import requests
+import json
+
+# url for inventory api
+inventoryApi = "https://inventory.dwsfties-uat-api.epa.gov"
+
+# copy and paste the access_token from your profile-> copy access token in UAT
+access_token = "<your_access_token>"
+
+headerForApi = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json"
+}
+
+# note you can change all the values to the right of :
+newWaterSystem = {
+  "waterSystemId": "X19852075",
+  "name": "myNewWaterSystem",
+  "wsOwnerTypeCode": "L",
+  "wsStatusCode": "A",
+  "waterSystemStatusDt": "2025-04-16",
+  "seasonalInd": "N",
+  "dwpWaterSystemAnnualOperatingPeriods": [],
+  "waterSystemServiceConnections": [],
+  "measureRequests": [],
+  "wsIndicatorRequests": [],
+  "relatedPOCRequests": [],
+  "flowRateRequests": [],
+  "waterSystemCertRequests": [],
+  "geographicAreas": [],
+  "serviceAreas": [],
+  "wholeSalerInd": "Y",
+  "wsServiceLineRequests": []
+}
+
+# Send the newWaterSystem
+apiCall = requests.post(f"{inventoryApi}/inventory/water-system", json=newWaterSystem, headers=headerForApi)
+
+# code of 200 means successfully added the newWaterSystem
+if apiCall.status_code != 200:
+    print("Api call failed with the token: ", apiCall.status_code, apiCall.text)
+else:
+    print("success")
